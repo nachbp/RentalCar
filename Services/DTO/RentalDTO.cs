11e37a86e83c83050507fe5ee0fdbcc870553bd6
nachbp <137377213+nachbp@users.noreply.github.com>
2023-08-10 00:04:46 +0200

    Add files via upload

--- Services/DTO/RentalDTO.cs
+++ Services/DTO/RentalDTO.cs
@@ -8,11 +8,21 @@ namespace Services.DTO
 {
     public class RentalDTO
     {
+        public RentalDTO(int id, List<int> carId, DateTime rentalDate, DateTime returnDate, int customerId, decimal totalPrice) 
+        {
+            this.Id = id;
+            this.CarId = carId;
+            this.RentalDate = rentalDate;
+            this.ReturnDate = returnDate;
+            this.CustomerId = customerId;
+            this.TotalPrice = totalPrice;            
+        }
         public int Id { get; set; }
-        public int CarId { get; set; }
+        public List<int> CarId { get; set; }
         public DateTime RentalDate { get; set; }
         public DateTime ReturnDate { get; set; }
         public int CustomerId { get; set; }
         public decimal TotalPrice { get; set; }
+        public decimal DailyRentalRate { get; set; }
     }
 }
